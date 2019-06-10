38e5ca3cf54972827250ed68a2a09bd00c389472
Geoffrey Gaurano <geoffrey.gaurano@gmail.com>
2019-06-10 19:09:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,16 +6,14 @@ new_arr = []
     return "The line is currently empty."
   end
   
- 
-        arr.each_with_index do |name, i|  
-          number = (i+1).to_s 
-          period = "."
-          person_phrase = (number + period + " " + name)
-          new_arr << person_phrase 
- 
-end 
+  arr.each_with_index do |name, i|  
+    number = (i+1).to_s #1. 
+    period = "."
+    person_phrase = (number + period + " " + name) #1. Adam 
+    new_arr << person_phrase #["1. Adam", "2. Geoffrey"]
+  end 
   
-list_of_names = new_arr.join(" ")
+list_of_names = new_arr.join(" ") #["1. Adam", "2. Geoffrey"] => "1. "
 
 
 final_phrase = "The line is currently:" + " " + list_of_names 
@@ -49,6 +47,22 @@ def take_a_number(arr, name)
   puts "Welcome," + " " + name + "." + " " + "You are number" + " " + number + " " + "in line."
 end
 
+ticket_number = 0 
+def take_a_number(arr)
+  
+ ticket_number = ticket_number + 1 
+ arr << ticket_number 
+
+return "You are ticket number" + ticket_number 
+end 
+
+# input: arr
+# output: You are ticket number #ticket_number 
+#[1, 2, 3] << 4
+# [1] => [1, 2] => [2, 2]
+# initital value = 0 
+# + 1 
+
 #take_a_number
 #input: an array and a string (someone's name)
 #output: A string containg the person's name and their index in the array
@@ -65,7 +79,7 @@ def now_serving(arr)
   if arr.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    next_person = (arr[0]).to_s 
+    next_person = (arr[0]).to_s #arr = [Adam, Geoffrey, Samantha] arr[0] = Adam
     puts "Currently serving" + " " + next_person + "."
   end
   
@@ -74,7 +88,7 @@ end
 
 #now_serving
 # input: array
-# output: the |name| at katz_deli[0]
+# output: the name at katz_deli[0]
 
 # if arr.length == 0 
     # puts "There is nobody waiting to be served!"
